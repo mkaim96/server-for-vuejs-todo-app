e70913bb5b714c27355e3c417772140f0ed274f5
Marcin <marcinkaim96@gmail.com>
2018-12-14 20:24:31 +0100

    refactored Logout

--- TodoServer/Controllers/TodosApiController.cs
+++ TodoServer/Controllers/TodosApiController.cs
@@ -127,7 +127,7 @@ namespace TodoServer.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
 
 
